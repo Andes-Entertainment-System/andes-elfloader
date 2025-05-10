6b4c7dca6818e4164524e3c8e617b14a020351e7
SneezingCactus <sneezingcactus452@gmail.com>
2025-05-10 01:47:43 -0300

    reformat code

--- unaligned.h
+++ unaligned.h
@@ -1,11 +1,11 @@
 /* This file was automatically generated.  Do not edit! */
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
-void unalignedCpy(void *dest,void *src,size_t n);
-void unalignedSet32(void *dest,uint32_t value);
+void unalignedCpy(void *dest, void *src, size_t n);
+void unalignedSet32(void *dest, uint32_t value);
 uint32_t unalignedGet32(void *src);
-void unalignedSet8(void *dest,uint8_t value);
+void unalignedSet8(void *dest, uint8_t value);
 uint8_t unalignedGet8(void *src);
 #define INTERFACE 0
