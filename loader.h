4f7d596b0061f80a584e08f5eaadfb60e1544f54
SneezingCactus <sneezingcactus452@gmail.com>
2025-05-16 23:16:31 -0300

    added struct keyword to typedefs

--- loader.h
+++ loader.h
@@ -9,13 +9,14 @@
 struct ELFLoaderSymbol_t {
   const char *name; /*!< Name of symbol */
   void *ptr;        /*!< Pointer of symbol in memory */
-} typedef ELFLoaderSymbol_t ELFLoaderSymbol_t;
+};
+typedef struct ELFLoaderSymbol_t ELFLoaderSymbol_t;
 
 struct ELFLoaderEnv_t {
   const ELFLoaderSymbol_t *exported; /*!< Pointer to exported symbols array */
   unsigned int exported_size;        /*!< Elements on exported symbol array */
 };
-typedef ELFLoaderEnv_t ELFLoaderEnv_t;
+typedef struct ELFLoaderEnv_t ELFLoaderEnv_t;
 
 struct ELFLoaderSection_t {
   void *dataHeap;
@@ -23,9 +24,9 @@ struct ELFLoaderSection_t {
   int secIdx;
   size_t size;
   off_t relSecIdx;
-  ELFLoaderSection_t *next;
+  struct ELFLoaderSection_t *next;
 };
-typedef ELFLoaderSection_t ELFLoaderSection_t;
+typedef struct ELFLoaderSection_t ELFLoaderSection_t;
 
 struct ELFLoaderContext_t {
   LOADER_FD_T fd;
@@ -43,7 +44,7 @@ struct ELFLoaderContext_t {
 
   ELFLoaderSection_t *section;
 };
-typedef ELFLoaderContext_t ELFLoaderContext_t;
+typedef struct ELFLoaderContext_t ELFLoaderContext_t;
 
 void *elfLoaderGetFunc(ELFLoaderContext_t *ctx, const char *funcname);
 ELFLoaderContext_t *elfLoaderInitLoadAndRelocate(LOADER_FD_T fd, const ELFLoaderEnv_t *env);
