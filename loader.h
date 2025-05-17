ffb94d58689cea01dd45edeb0cfcd303713cfeab
SneezingCactus <sneezingcactus452@gmail.com>
2025-05-16 23:14:30 -0300

    reformat structs

--- loader.h
+++ loader.h
@@ -6,26 +6,28 @@
 #define LOADER_FD_T void *
 #define ELFLOADER_EXPORT_SYMBOL(name) {#name, (void *)name}
 
-typedef struct {
+struct ELFLoaderSymbol_t {
   const char *name; /*!< Name of symbol */
   void *ptr;        /*!< Pointer of symbol in memory */
-} ELFLoaderSymbol_t;
+} typedef ELFLoaderSymbol_t ELFLoaderSymbol_t;
 
-typedef struct {
+struct ELFLoaderEnv_t {
   const ELFLoaderSymbol_t *exported; /*!< Pointer to exported symbols array */
   unsigned int exported_size;        /*!< Elements on exported symbol array */
-} ELFLoaderEnv_t;
+};
+typedef ELFLoaderEnv_t ELFLoaderEnv_t;
 
-typedef struct {
+struct ELFLoaderSection_t {
   void *dataHeap;
   void *dataExec;
   int secIdx;
   size_t size;
   off_t relSecIdx;
   ELFLoaderSection_t *next;
-} ELFLoaderSection_t;
+};
+typedef ELFLoaderSection_t ELFLoaderSection_t;
 
-typedef struct {
+struct ELFLoaderContext_t {
   LOADER_FD_T fd;
   void *exec;
   void *text;
@@ -40,7 +42,8 @@ typedef struct {
   off_t strtab_offset;
 
   ELFLoaderSection_t *section;
-} ELFLoaderContext_t;
+};
+typedef ELFLoaderContext_t ELFLoaderContext_t;
 
 void *elfLoaderGetFunc(ELFLoaderContext_t *ctx, const char *funcname);
 ELFLoaderContext_t *elfLoaderInitLoadAndRelocate(LOADER_FD_T fd, const ELFLoaderEnv_t *env);
